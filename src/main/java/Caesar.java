4466c253ffb4b36326747548be4c0e386da8a3fb
Zara <24zalam@athenian.org>
2021-09-19 13:36:08 -0700

    listening to music utilizes the entire brain.

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -5,33 +5,120 @@ public class Caesar {
     // and returns the encrypted String
     // Ex. encryptCaesar("Hello, World!") => "Khoor, Zruog!"
     public static String encryptCaesar(String message) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
-    }
+        String newss="";
+        String alphabet="abcdefghijklmnopqrstuvwxyz";
+        String alph2="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for(int i=0; i<=message.length()-1;i++) {
+            int smth = alphabet.indexOf(message.charAt(i));
+            if (smth==-1) {
+                smth = alph2.indexOf(message.charAt(i));
+            if (smth == -1) {
+                newss=newss+message.charAt(i);
+                continue;
+                }
+                int smth2 = (3+smth) % 26;
+                char ch = alph2.charAt(smth2);
+                newss = newss + ch;
+                continue;
+            }
+            int smth2 = (3+smth) % 26;
+            char ch = alphabet.charAt(smth2);
+            newss = newss + ch;
+        }
+        return newss;
+        }
+
+
+
 
     // Given a String, decrypts the String with the Caesar cipher
     // and returns the original String
     // Ex. decryptCaesar("Khoor, Zruog!") => "Hello, World!"
     public static String decryptCaesar(String message) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String news="";
+        String alphabet="abcdefghijklmnopqrstuvwxyz";
+        String alph2="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for(int i=0; i<=message.length()-1;i++) {
+            int smth = alphabet.indexOf(message.charAt(i));
+            if (smth==-1) {
+                smth = alph2.indexOf(message.charAt(i));
+                if (smth == -1) {
+                    news=news+message.charAt(i);
+                    continue;
+                }
+                int smth2 = (smth-3) % 26;
+                char ch = alph2.charAt(smth2);
+                news = news + ch;
+                continue;
+            }
+            int smth2 = (smth-3) % 26;
+            char ch = alphabet.charAt(smth2);
+            news = news + ch;
+        }
+        return news;
     }
 
     // Given a String and a key corresponding to a shift, encrypts
     // the String using the given key and returns the encrypted String
     // Ex. encryptCaesarKey("Hello, World!", 5) => "Mjqqt, Btwqi!".
     public static String encryptCaesarKey(String message, int key) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String newss="";
+        String alphabet="abcdefghijklmnopqrstuvwxyz";
+        String alph2="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for(int i=0; i<=message.length()-1;i++) {
+            int smth = alphabet.indexOf(message.charAt(i));
+            if (smth==-1) {
+                smth = alph2.indexOf(message.charAt(i));
+                if (smth == -1) {
+                    newss=newss+message.charAt(i);
+                    continue;
+                }
+                int smth2 = (key+smth) % 26;
+                char ch = alph2.charAt(smth2);
+                newss = newss + ch;
+                continue;
+            }
+            int smth2 = (key+smth) % 26;
+            char ch = alphabet.charAt(smth2);
+            newss = newss + ch;
+        }
+        return newss;
     }
+
+
 
     // Given a String and a key corresponding to a shift, decrypts
     // the String using the given key and returns the original String
     // Ex. decryptCaesarKey("Mjqqt, Btwqi!", 5) => "Hello, World!"
     public static String decryptCaesarKey(String message, int key) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String news="";
+        String alphabet="abcdefghijklmnopqrstuvwxyz";
+        String alph2="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        for(int i=0; i<=message.length()-1;i++) {
+            int smth = alphabet.indexOf(message.charAt(i));
+            if (smth==-1) {
+                smth = alph2.indexOf(message.charAt(i));
+                if (smth == -1) {
+                    news=news+message.charAt(i);
+                    continue;
+                }
+                int smth2 = (smth-key) % 26;
+                char ch = alph2.charAt(smth2);
+                news = news + ch;
+                continue;
+            }
+            int smth2 = (smth-key) % 26;
+            char ch = alphabet.charAt(smth2);
+            news = news + ch;
+        }
+        return news;
     }
+
+
 
 
     // The main method is already provided for you
